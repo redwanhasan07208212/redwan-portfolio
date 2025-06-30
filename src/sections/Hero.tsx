fe6b265e0acadd3e4cd2200c4faa3785549c0b22
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-06-30 13:03:13 +0600

    hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,11 @@
+import redwan from "@/assets/images/redwan.jpg";
+import Image from "next/image";
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div>
+      <div className="container">
+        <Image src={redwan} alt="redwan hasan" height={424} width={424} />
+      </div>
+    </div>
+  );
 };
