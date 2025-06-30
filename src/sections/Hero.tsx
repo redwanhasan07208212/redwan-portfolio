a7866ed64f9dd10eb1d19254b9874851d0bb361b
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-06-30 15:47:15 +0600

    working on hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,39 @@
+import React from "react";
 import redwan from "@/assets/images/redwan.jpg";
+import { ArrowDown } from "lucide-react";
 import Image from "next/image";
 export const HeroSection = () => {
   return (
-    <div>
+    <div className="py-32">
       <div className="container">
-        <Image src={redwan} alt="redwan hasan" height={424} width={424} />
+        <div className="flex flex-col items-center">
+          <Image
+            src={redwan}
+            alt="redwan hasan"
+            className="size-[100px] rounded-lg"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Available for new Projects
+            </div>
+          </div>
+        </div>
+        <h1>Building Exceptional User Experience</h1>
+        <p>
+          I specialize in transforming designs into functional, high-performing
+          web applications. Lets discuss you next Project.
+        </p>
+        <div>
+          <button>
+            <span>Explore My Work</span>
+            <ArrowDown />
+          </button>
+          <button>
+            <span>👋</span>
+            <span>Let&apos;s Connect</span>
+          </button>
+        </div>
       </div>
     </div>
   );
