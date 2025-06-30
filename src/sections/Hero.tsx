737429a91832ef4252b17a4d61961c2ee0f29153
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-07-01 00:31:32 +0600

    hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import redwan from "@/assets/images/redwan.jpg";
 import { ArrowDown } from "lucide-react";
 import Image from "next/image";
+import grainImg from "@/assets/images/grain.jpg";
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 flex flex-col items-center justify-center">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{ backgroundImage: `url(${grainImg.src})` }}
+      ></div>
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
@@ -19,19 +28,23 @@ export const HeroSection = () => {
             </div>
           </div>
         </div>
-        <h1>Building Exceptional User Experience</h1>
-        <p>
-          I specialize in transforming designs into functional, high-performing
-          web applications. Lets discuss you next Project.
-        </p>
-        <div>
-          <button>
-            <span>Explore My Work</span>
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experience
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialize in transforming designs into functional,
+            high-performing web applications. Lets discuss you next Project.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2  border border-white/15 rounded-xl px-6 h-12">
+            <span className="font-semibold">Explore My Work</span>
             <ArrowDown />
           </button>
-          <button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋</span>
-            <span>Let&apos;s Connect</span>
+            <span className="font-semibold">Let&apos;s Connect</span>
           </button>
         </div>
       </div>
